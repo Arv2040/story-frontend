afc3339a8e0a053e50f458ad7f8cac0d9109384b
Vibhor <vibhorag2003@gmail.com>
2024-02-22 21:59:25 +0530

    scrollbar works and is now hidden

--- src/App.js
+++ src/App.js
@@ -1,26 +1,31 @@
-import './App.css';
-import 'tailwindcss/tailwind.css';
-import Prompt from './components/Prompt';
-import Options from './components/Options';
-import Question from './components/Question';
-
+import "./App.css";
+import "tailwindcss/tailwind.css";
+import Prompt from "./components/Prompt";
+import Options from "./components/Options";
+import Question from "./components/Question";
 
 function App() {
   return (
-    <main className ="h-screen bg-black">
-      <section className = "flex flex-col justify-evenly items-center">
-      <h1 className="text-white  w-fit text-5xl font-sans font-bold ">Story.ai</h1>
-      
-      <Prompt/>
-      {/* <Options/> */}
-      {/* <Question/> */}
-      
-      
-      <div className="">
-        <input type="text" name="" id="" placeholder = "Enter your prompt here" className = "text-center rounded-md w-96 p-2 absolute bottom-4 left-[50%] translate-x-[-50%]"/>
-      </div>
+    <main className="h-screen bg-black py-8">
+      <section className="flex flex-col justify-evenly items-center">
+        <h1 className="text-white w-fit text-5xl font-sans font-bold ">
+          Story.ai
+        </h1>
+
+        <Prompt />
+        {/* <Options/> */}
+        {/* <Question/> */}
+
+        <div className="">
+          <input
+            type="text"
+            name=""
+            id=""
+            placeholder="Enter your prompt here"
+            className="text-center rounded-md w-96 p-2 absolute bottom-4 left-[50%] translate-x-[-50%]"
+          />
+        </div>
       </section>
-     
     </main>
   );
 }
