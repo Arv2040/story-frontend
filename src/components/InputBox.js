881ffb82da2290c18798eeedf19e4912dca6a117
Vibhor <vibhorag2003@gmail.com>
2024-02-22 20:56:50 +0530

    feat: buttons added in options

---
+++ src/components/InputBox.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function InputBox() {
+  return (
+    <input
+      type="text"
+      name=""
+      id=""
+      placeholder="This is where you enter your prompt"
+      className="text-center rounded-md w-256 p-2"
+    />
+  );
+}
--- src/components/Options.js
+++ src/components/Options.js
@@ -1,7 +1,15 @@
-import React from 'react'
+import React from "react";
+import Button from "./Button";
 
 export default function Options() {
   return (
-    <div className = "text-white w-full border-2 border-white mt-4">Options</div>
-  )
+    <>
+      <div className="text-white w-full border-2 border-white mt-4 grid grid-cols-2 gap-x-16 gap-y-4">
+        <Button />
+        <Button />
+        <Button />
+        <Button />
+      </div>
+    </>
+  );
 }
