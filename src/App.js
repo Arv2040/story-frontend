ea2da7c17a9473d7f4f7353b5649bd3bcd4002c4
Vibhor <vibhorag2003@gmail.com>
2024-02-23 01:40:06 +0530

    cursor

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import "./App.css";
 import React, { useState } from "react";
+import { useTypewriter, Cursor } from "react-simple-typewriter";
+import "./App.css";
 import "tailwindcss/tailwind.css";
 import Question from "./components/Question";
 import Content from "./components/Content";
@@ -7,11 +8,13 @@ import Options from "./components/Options";
 
 function App() {
   const [change, setChange] = useState(false);
+
   return (
     <main className="h-screen bg-black py-8">
       <section className="flex flex-col justify-evenly items-center">
-        <h1 className="text-white w-fit text-5xl merriweather-light">
-          Story.ai
+        <h1 className="text-white w-auto text-5xl merriweather-light">
+          StoryForge.ai
+          <Cursor />
         </h1>
 
         {/* <Content /> */}
